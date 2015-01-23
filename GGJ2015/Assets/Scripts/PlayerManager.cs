ecd8708fab8223a9c2b37a29f4f267a3ffa5cd37
Nick Cooper <envy3d@gmail.com>
2015-01-24 04:57:50 +0800

    Added basic player UI

---
+++ GGJ2015/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+
+    [SerializeField]
+    private int numberOfPlayers;
+    [SerializeField]
+    private int numberOfFirstLevel = 1;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        OnLevelWasLoaded(2);
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level >= numberOfFirstLevel)
+        {
+            for (int playerNum = 0; playerNum < numberOfPlayers; playerNum++)
+            {
+                GameObject player = Instantiate(playerPrefab) as GameObject;
+                player.BroadcastMessage("ReceivePlayerNumber", playerNum);
+            }
+        }
+    }
+}
---
+++ GGJ2015/Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerUI : MonoBehaviour {
+
+    public GameObject playerUiPrefab;
+
+    private GameObject playerUI;
+
+    public void ReceivePlayerNumber(int playerNumber)
+    {
+        GameObject board = GameObject.FindGameObjectWithTag("GameBoard");
+        float boardWidth = board.GetComponent<RectTransform>().rect.width;
+
+        playerUI = GameObject.Instantiate(playerUiPrefab) as GameObject;
+        var playerUiRectTransform = playerUI.GetComponent<RectTransform>();
+        playerUI.transform.SetParent(board.transform);
+        playerUiRectTransform.localScale = new Vector3(1, 1, 1);
+        playerUiRectTransform.offsetMin = new Vector2((playerNumber) * (boardWidth / 4), 0);
+        playerUiRectTransform.offsetMax = new Vector2(-(3 - playerNumber) * (boardWidth / 4), 0);
+        //playerUiRectTransform.rect.Set(0, 0, 256, playerUiRectTransform.rect.height);
+        playerUI.transform.SetParent(board.transform);
+
+        //var playerUiRectTransform = playerUI.GetComponent<RectTransform>();
+
+
+       // playerUiRectTransform.rect.Set(0, 0, 256, playerUiRectTransform.rect.height);
+        /*playerUiRectTransform.rect.Set((-boardWidth/2) + ((playerNumber - 1) * (boardWidth / 4)),
+                                        playerUiRectTransform.rect.yMin,
+                                        boardWidth / 4,
+                                        playerUiRectTransform.rect.height);*/
+    }
+}
