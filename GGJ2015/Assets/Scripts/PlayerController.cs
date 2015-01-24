c4ee1719255b219dce69165d0d99645279554fb2
r4gn4r0k0n <casketlim@gmail.com>
2015-01-24 14:04:20 +0800

    PlayerController Script is somewhat done

--- GGJ2015/Assets/Scripts/PlayerController.cs
+++ GGJ2015/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameManager gm;
+	public int[] playerSkillSetIdx;
+	public Skill[] playerSkillSet;
+
+	public KeyCode key1;
+	public KeyCode key2;
+	public KeyCode key3;
+	public KeyCode key4;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(key1)){
+			gm.UseSkill(playerSkillSetIdx[0]);
+		}
+		if(Input.GetKeyDown(key2)){
+			gm.UseSkill(playerSkillSetIdx[1]);
+		}
+		if(Input.GetKeyDown(key3)){
+			gm.UseSkill(playerSkillSetIdx[2]);
+		}
+		if(Input.GetKeyDown(key4)){
+			gm.UseSkill(playerSkillSetIdx[3]);
+		}
+	}
+
+	public Skill RetrievePlayerSkills (int skillIdx) {
+		return playerSkillSet[skillIdx];
 	}
 }
